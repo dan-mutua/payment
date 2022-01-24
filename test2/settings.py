7f6f597a7227c2d9e537a659b2ed83ee5e61243e
dan-mutua <daniel.mutua@student.moringaschool.com>
2022-01-24 15:01:44 +0300

    first commit

--- test2/settings.py
+++ test2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u%c*%v@--u((g9a$teut-w7c*^*)h#*!-y!kh)3!b+kho+i5_+'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
